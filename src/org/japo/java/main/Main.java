ca7989fbf7add3753703f2b94db6e30de931cf6b
José A. Pacheco Ondoño <joanpaon@gmail.com>
2017-10-05 14:26:59 +0200

    :tada: First version

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,60 @@
+/* 
+ * Copyright 2016 José A. Pacheco Ondoño - joanpon@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author José A. Pacheco Ondoño - joanpon@gmail.com
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Instanciar Scanner
+        Scanner scn = new Scanner(System.in);
+
+        // Variable
+        double altura;
+
+        // Aplicar Convención Cultural
+        scn.useLocale(Locale.ENGLISH);
+
+        // Cuarentena
+        try {
+            // Leer dato
+            System.out.print("Mi altura es .............: ");
+            altura = scn.nextDouble();
+
+            // Borrar buffer
+            scn.nextLine();
+
+            // Mensaje
+            System.out.printf(Locale.ENGLISH,
+                    "Mi altura es .............: %.2f metros\n", altura);
+        } catch (Exception e) {
+            // Mensaje
+            System.out.println("Entrada incorrecta");
+
+            // Borrar buffer
+            scn.nextLine();
+        }
+    }
+}
